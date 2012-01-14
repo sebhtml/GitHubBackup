09d90ec3aad418a031bf441693fef8b2eff651dd
Sébastien Boisvert <sebastien.boisvert.3@ulaval.ca>
2012-01-14 13:34:46 -0500

    Added a README.

--- GitHubBackup.sh
+++ GitHubBackup.sh
@@ -1,4 +1,12 @@
-user=sebhtml
+#!/bin/bash
+# \author Sébastien Boisvert
+
+user=$1
+
+
+mkdir -p repositories-$user
+
+# fetch the list
 
 curl http://github.com/api/v2/json/repos/show/$user > data.txt
 
@@ -12,22 +20,32 @@ done|grep 'name":' > list.txt
 sed -i 's/"/ /g' list.txt
 sed -i 's/name :/ /g' list.txt
 
+# convert the list
+
 for i in $(cat list.txt)
 do
 	echo $i
 done > projects.txt
 
+# clone each repository
+
 for i in $(cat projects.txt)
 do
-	cd repositories
+	cd repositories-$user
 	git clone git://github.com/$user/$i.git
 	cd ..
 done
 
+# pull each repository
+
 for i in $(cat projects.txt)
 do
-	cd repositories/$i
+	cd repositories-$user/$i
 	git pull
 	cd ..
 	cd ..
 done
+
+# add an entry in the log
+
+date >> Log.txt
